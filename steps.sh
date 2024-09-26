a6d2865827c66d9ed552d47b90b4a7b6878a6571
welasco <26201510+Welasco@users.noreply.github.com>
2024-09-26 17:15:54 -0500

    Add Jekyll

--- steps.sh
+++ steps.sh
@@ -74,3 +74,27 @@ helm install syncsecretakv syncsecretakv/syncsecretakv --set namespace=syncsecre
 
 # Uninstall Helm Chart
 helm uninstall syncsecretakv
+
+# Set up the GitHub Pages
+# Install Jekyll
+sudo apt-get install ruby-full build-essential zlib1g-dev
+gem install jekyll bundler
+
+# Create a new Jekyll site
+jekyll new syncsecretakv
+
+# Build the site and make it available on a local server
+cd jekyll-syncsecretakv
+bundle exec jekyll serve --host=0.0.0.0
+
+# Now you can browse to http://localhost:4000
+# Stop the server by pressing Ctrl+C
+
+# I customized the theme by changing the _config.yml file
+# I also replaced the main page with index.markdown
+# At the end it's 3 files, _config.yml, index.markdown and Gemfile
+# Setup github actions: https://jekyllrb.com/docs/continuous-integration/github-actions/
+
+# Install changes
+bundle install
+
